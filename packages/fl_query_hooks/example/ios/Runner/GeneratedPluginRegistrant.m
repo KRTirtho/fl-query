a28cc2801c7a252c6af447cebd48bccc3ec5bb85
Kingkor Roy Tirtho <krtirtho@gmail.com>
2022-08-07 12:14:56 +0600

    chore: merge main

---
+++ packages/fl_query_hooks/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<connectivity_plus/ConnectivityPlusPlugin.h>)
+#import <connectivity_plus/ConnectivityPlusPlugin.h>
+#else
+@import connectivity_plus;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ConnectivityPlusPlugin registerWithRegistrar:[registry registrarForPlugin:@"ConnectivityPlusPlugin"]];
+}
+
+@end
---
+++ packages/fl_query_hooks/example/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/opt/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/krtirtho/dev/fl-query/packages/fl_query_hooks/example" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/opt/flutter"
+  "PROJECT_DIR=/home/krtirtho/dev/fl-query/packages/fl_query_hooks/example"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/krtirtho/dev/fl-query/packages/fl_query_hooks/example/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/krtirtho/dev/fl-query/packages/fl_query_hooks/example/lib/main.dart"
+)
