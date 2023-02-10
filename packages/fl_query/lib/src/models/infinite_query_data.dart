f632fe84039980812478ee014b6dae1e5b15efd5
Kingkor Roy Tirtho <krtirtho@gmail.com>
2023-02-10 19:08:30 +0600

    feat: clean old junk

--- packages/fl_query/lib/src/models/infinite_query_data.dart
+++
@@ -1,9 +0,0 @@
-class InfiniteQueryData<T extends Object> {
-  final Set<T> pages;
-  final Set<String> pageParams;
-
-  InfiniteQueryData({
-    required this.pages,
-    required this.pageParams,
-  });
-}
--- packages/fl_query_hooks/lib/fl_query_hooks.dart
+++ packages/fl_query_hooks/lib/fl_query_hooks.dart
@@ -1,5 +1,1 @@
 library fl_query_hooks;
-
-export 'src/use_query.dart';
-export 'src/use_mutation.dart';
-export 'src/use_infinite_query.dart';
