121c996903bfdf58f13fe9a912f8a58f4a81dfd8
Kingkor Roy Tirtho <krtirtho@gmail.com>
2023-03-05 13:18:20 +0600

    chore: remove unneeded deps from lock files

--- packages/fl_query_hooks/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/fl_query_hooks/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_foundation
+import connectivity_plus_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
 }
