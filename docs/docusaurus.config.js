930164cda8d909853a61f12d9b3a8884d719102a
Kingkor Roy Tirtho <krtirtho@gmail.com>
2023-10-06 21:43:05 +0600

    chore: update docs deps

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
   
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const { themes } = require('prism-react-renderer');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -133,8 +132,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Kingkor Roy Tirtho`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: themes.oneLight,
+        darkTheme: themes.oneDark,
         additionalLanguages: ["dart"]
       },
     }),
